a8b2ff89833565b10c7641f329ee202fb44ef106
Hawshemi <rhawshemi@gmail.com>
2023-09-02 12:55:48 +0330

    Added Assignments.
    Added Assignments.

---
+++ 4. Tools of the Trade Linux and SQL/Week 4/Activity - Filter with AND, OR, and NOT.sql
@@ -0,0 +1,11 @@
+SELECT * FROM log_in_attempts WHERE login_time > '18:00' AND success = FALSE;
+
+SELECT * FROM log_in_attempts WHERE login_date = '2022-05-09' OR login_date = '2022-05-08';
+
+SELECT * FROM log_in_attempts WHERE NOT country LIKE 'MEX%';
+
+SELECT * FROM employees WHERE department = 'Marketing' AND office LIKE 'East-%';
+
+SELECT * FROM employees WHERE department = 'Finance' OR department = 'Sales';
+
+SELECT * FROM employees WHERE NOT department = 'Information Technology';
